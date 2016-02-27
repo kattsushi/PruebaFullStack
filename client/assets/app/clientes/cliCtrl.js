6dd161b126fa90c1979b4d32c6edf79baa6c3d0a
kattsushi <ndresdavidj@gmail.com>
2016-02-26 22:49:26 -0430

    Crud Clientes v1

--- client/assets/app/clientes/cliCtrl.js
+++ client/assets/app/clientes/cliCtrl.js
@@ -20,35 +20,39 @@
     
     var uid = 1;       
      
-    $scope.contacts = [
-        {id:0, 'name': 'PremAseem', 'email':'hello@gmail.com', 'phone': '123-2343-44'}
+    $scope.clientes = [
+        {id:0, 'documento': '20282731', 'nombres':'Andres Jimenez', 'detalles': '+58-426-1637-863'}
     ];
     
+
     
-    $scope.saveContact = function() {
+    $scope.saveCliente = function() {
         
-        if($scope.newcontact.id == null) {
-             $scope.newcontact.id = uid++;
-             $scope.contacts.push($scope.newcontact);
+        if($scope.newCliente.id == null) {
+             $scope.newCliente.id = uid++;
+             $scope.clientes.push($scope.newCliente);
+             $scope.numeroDeclientes = $scope.clientes.length;
+             console.log($scope.numeroDeclientes);
         } else {
             
-             for(var i in $scope.contacts) {
-                    if($scope.contacts[i].id == $scope.newcontact.id) {
-                        $scope.contacts[i] = $scope.newcontact;
+             for(var i in $scope.clientes) {
+                    if($scope.clientes[i].id == $scope.newCliente.id) {
+                        $scope.clientes[i] = $scope.newCliente;
                     }
              }                
         }
-        console.log($scope.contacts)
-        $scope.newcontact = {};
+        $scope.newCliente = {};
     }
 
     
     $scope.delete = function(id) {
         
-        for(var i in $scope.contacts) {
-            if($scope.contacts[i].id == id) {
-                $scope.contacts.splice(i,1);
-                $scope.newcontact = {};
+        for(var i in $scope.clientes) {
+            if($scope.clientes[i].id == id) {
+                $scope.clientes.splice(i,1);
+                $scope.newCliente = {};
+                $scope.numeroDeclientes = $scope.clientes.length;
+                console.log($scope.numeroDeclientes);
             }
         }
         
@@ -56,20 +60,46 @@
     
     
     $scope.edit = function(id) {
-        for(var i in $scope.contacts) {
-            if($scope.contacts[i].id == id) {
-                $scope.newcontact = angular.copy($scope.contacts[i]);
+        for(var i in $scope.clientes) {
+            if($scope.clientes[i].id == id) {
+                $scope.newCliente = angular.copy($scope.clientes[i]);
             }
         }
-    }        
+    }   
+
+
+     $scope.pageSize     = 5;
+     $scope.currentPage  = 0;
+     
+      
+     $scope.numeroDePag = function () {
+         
+         if ($scope.numeroDeclientes > $scope.pageSize) {
+            return Math.ceil( $scope.numeroDeclientes / $scope.pageSize);            
+         }else{
+             return 1;
+         }
+       }
+
+         
             
             
     //------------------------------------------------------------------------------------------------------
     }
+    
+    function cliFilt () {
+        return function (input, start) {
+         start = +start;
+        return input.slice(start);
+    }
+  };
+
+        
       angular.module('App')
              .controller('cliCtrl',['$scope', '$timeout','$mdSidenav',
                                      '$rootScope', 'cliServ', '$mdDialog',
                                      '$mdMedia', '$cookieStore','$location',
                                      '$http', '$q', '$log',
-                                     cliCtrl]);
+                                     cliCtrl])
+             .filter('startFrom',[cliFilt]);
 })();
